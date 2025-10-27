326da57ae65b9651ee1d277a8333a602bb9a40f4
dariosanacuore <dariosanacuore2@gmail.com>
2025-10-27 15:58:21 +0100

    feat:Aggiunta di altri controlli sull'input

--- js/script.js
+++ js/script.js
@@ -17,24 +17,31 @@ let prezzo = numKm * 0.21;
 let scontoMinorenni = (prezzo * 20) / 100;
 let scontoOver65 = (prezzo * 40) / 100;
 
-if (eta < 0 || numKm < 0) {
+
+if (eta < 0 || numKm <= 0) {
     console.log("Dati inseriti non sono validi");
+} else if (Number.isNaN(eta) || Number.isNaN(numKm)) {
+    console.log("Errore: inserisci solo numeri validi");
+
 } else {
     if (eta < 18) {
         prezzoFinale = (prezzo - scontoMinorenni);
         prezzoFinale = Math.round(prezzoFinale * 100) / 100;
-        risultato = "Il prezzo finale è: " + prezzoFinale;
+        risultato = "Il prezzo finale è: " + prezzoFinale + " €";
 
     } else if (eta > 65) {
         prezzoFinale = (prezzo - scontoOver65);
         prezzoFinale = Math.round(prezzoFinale * 100) / 100;
-        risultato = "Il prezzo finale è: " + prezzoFinale;
+        risultato = "Il prezzo finale è: " + prezzoFinale + " €";
     } else {
         prezzoFinale = prezzo;
         prezzoFinale = Math.round(prezzoFinale * 100) / 100;
-        risultato = "Il prezzo finale è: " + prezzo;
+        risultato = "Il prezzo finale è: " + prezzo + " €";
     }
 }
+
+
+
 
 prezzoFinale = Math.round(prezzoFinale * 100) / 100;
 
