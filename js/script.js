58b59c812a66859f12d8bae46bd7536c9bcd9eb6
dariosanacuore <dariosanacuore2@gmail.com>
2025-10-27 15:44:48 +0100

    feat:stampa del risultato con massimo 2 decimali

--- js/script.js
+++ js/script.js
@@ -8,8 +8,8 @@
 //Calcolo la tariffa base moltiplicando il numero dei km per 0.21 euro
 //Poi applico gli sconti per le categorie di persone e calcolo il prezzo finale in base ai vari scenari sottraendo dal prezzo lo sconto
 
-const eta = prompt("Inserisci la tua età: ");
-const numKm = prompt("Inserisci Il numero di km da percorrere: ");
+const eta = parseInt(prompt("Inserisci la tua età: "));
+const numKm = parseFloat(prompt("Inserisci Il numero di km da percorrere: "));
 let prezzoFinale;
 let risultato = "";
 
@@ -17,15 +17,26 @@ let prezzo = numKm * 0.21;
 let scontoMinorenni = (prezzo * 20) / 100;
 let scontoOver65 = (prezzo * 40) / 100;
 
-if (eta < 18) {
-    prezzoFinale = prezzo - scontoMinorenni;
-    risultato = "Il prezzo finale è: " + prezzoFinale;
-
-} else if (eta > 65) {
-    prezzoFinale = prezzo - scontoOver65;
-    risultato = "Il prezzo finale è: " + prezzoFinale;
+if (eta < 0 || numKm < 0) {
+    console.log("Dati inseriti non sono validi");
 } else {
-    risultato = "Il prezzo finale è: " + prezzo;
+    if (eta < 18) {
+        prezzoFinale = (prezzo - scontoMinorenni);
+        prezzoFinale = Math.round(prezzoFinale * 100) / 100;
+        risultato = "Il prezzo finale è: " + prezzoFinale;
+
+    } else if (eta > 65) {
+        prezzoFinale = (prezzo - scontoOver65);
+        prezzoFinale = Math.round(prezzoFinale * 100) / 100;
+        risultato = "Il prezzo finale è: " + prezzoFinale;
+    } else {
+        prezzoFinale = prezzo;
+        prezzoFinale = Math.round(prezzoFinale * 100) / 100;
+        risultato = "Il prezzo finale è: " + prezzo;
+    }
 }
+
+prezzoFinale = Math.round(prezzoFinale * 100) / 100;
+
 
 console.log(risultato);
